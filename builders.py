e1e2210c8d32798bd1de4ef37dbf36928df4822f
Fabio Alessandrelli <fabio.alessandrelli@gmail.com>
2022-04-22 14:47:56 +0200

    okay this fails, what about upstream.

--- builders.py
+++ builders.py
@@ -92,9 +92,9 @@ def build_rtc(env, build_dir, source_dir):
         "-DNO_TESTS=1",
         "-DOPENSSL_USE_STATIC_LIBS=1",
         "-DOPENSSL_ROOT_DIR=%s" % get_ssl_build_dir(env),
-        "-DOPENSSL_INCLUDE_DIR=%s" % get_ssl_include_dir(env),
-        "-DOPENSSL_SSL_LIBRARY=ssl",
-        "-DOPENSSL_CRYPTO_LIBRARY=crypto",
+        #"-DOPENSSL_INCLUDE_DIR=%s" % get_ssl_include_dir(env),
+        #"-DOPENSSL_SSL_LIBRARY=ssl",
+        #"-DOPENSSL_CRYPTO_LIBRARY=crypto",
     ]
     if env["platform"] == "android":
         abi = {
