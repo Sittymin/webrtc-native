76fbd9ff5e98374aa106d27ef160c0cca8d44dfb
Fabio Alessandrelli <fabio.alessandrelli@gmail.com>
2022-04-22 22:04:27 +0200

    fix osx, BREAKS the rest?

--- builders.py
+++ builders.py
@@ -107,7 +107,6 @@ def build_rtc(env, build_dir, source_dir):
         "-DNO_WEBSOCKET=1",
         "-DNO_TESTS=1",
         "-DOPENSSL_USE_STATIC_LIBS=1",
-        "-DOPENSSL_ROOT_DIR=%s" % get_ssl_build_dir(env),
         "-DOPENSSL_INCLUDE_DIR=%s" % get_ssl_include_dir(env),
         "-DOPENSSL_SSL_LIBRARY=%s/libssl.a" % get_ssl_build_dir(env),
         "-DOPENSSL_CRYPTO_LIBRARY=%s/libcrypto.a" % get_ssl_build_dir(env),
